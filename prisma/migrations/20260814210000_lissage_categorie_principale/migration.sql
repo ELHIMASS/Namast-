3984c1272726276b101d2434c01a796f7c471212
EL HIMASS <ismail.elhimass@gmail.com>
2026-08-14 20:52:14 +0200

    fix: séparer migration enum et update en deux transactions
    PostgreSQL ne permet pas d'utiliser une nouvelle enum value
    dans la même transaction que ALTER TYPE.
    
    Sépare en deux migrations:
    - 20260814210000: ALTER TYPE pour ajouter LISSAGE
    - 20260814211000: UPDATE les prestations pour utiliser LISSAGE

--- prisma/migrations/20260814210000_lissage_categorie_principale/migration.sql
+++ prisma/migrations/20260814210000_lissage_categorie_principale/migration.sql
@@ -1,7 +1,2 @@
 -- Ajouter LISSAGE à l'enum CategoriePrestation
 ALTER TYPE "CategoriePrestation" ADD VALUE 'LISSAGE' BEFORE 'HEAD_SPA';
-
--- Mettre à jour les prestations de lissage pour avoir LISSAGE comme catégorie
-UPDATE "Prestation"
-SET "categorie" = 'LISSAGE'
-WHERE "estLissage" = true;
